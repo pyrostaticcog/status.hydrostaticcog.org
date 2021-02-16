0edec7aaf574e46d5a8f4a21607c1a55d792fda1
hydrostaticcog <hydro@hydrostaticcog.org>
2021-02-16 12:54:05 -0600

    adds status backend

---
+++ scripts/mcping.js
@@ -0,0 +1,28 @@
+var request = new XMLHttpRequest()
+// Open a new connection, using the GET request on the URL endpoint
+request.open('GET', 'https://api.mcsrvstat.us/2/vegasmp.hydrostaticcog.org', true)
+
+request.onload = function () {
+    var data = JSON.parse(this.response)
+    if (data.online) {
+        if (!data.players.list) {
+            var playerList = 'No Players Online'
+        } else {
+            var playerList = data.players.list
+        }
+        document.getElementById("status-mc").innerHTML = 'Online';
+        document.getElementById('status-mc').style.backgroundColor = "green";
+        document.getElementById("status-gh").innerHTML = 'Online';
+        document.getElementById('status-gh').style.backgroundColor = "green";
+
+    }
+    else {
+        document.getElementById("status-mc").innerHTML = 'Offline'
+        document.getElementById('status-mc').style.backgroundColor = "red";
+        document.getElementById("status-gh").innerHTML = 'Offline'
+        document.getElementById('status-gh').style.backgroundColor = "red";
+    }
+}
+
+// Send request
+request.send()
